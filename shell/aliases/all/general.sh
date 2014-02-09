7b6c675c1a77e2644c2b1502a694c29b329db593
jeroentbt <jeroen@tinktenk.be>
2014-02-09 11:42:59 +0100

    Split off aliases for osx, fix #2

---
+++ shell/aliases/all/general.sh
@@ -0,0 +1,3 @@
+alias em="emacsclient -t"
+alias ema="emacsclient -c"
+alias ls='ls -GFh'
--- shell/aliases/osx/general.sh
+++ shell/aliases/osx/general.sh
@@ -1,6 +1,2 @@
-alias em="emacsclient -t"
-alias ema="emacsclient -c"
 alias rm='trash'
-alias ls='ls -GFh'
 alias pd='/Applications/Pd-extended.app/Contents/Resources/bin/pd'
-
